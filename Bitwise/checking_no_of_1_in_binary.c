6254433054c3e5345d1ee0b0b6f9272f21ea0d95
Abhay-Maurya <technicallysmart1@gmail.com>
2023-01-11 19:24:06 +0530

    counting no of 1's in integer in binary form

---
+++ Bitwise/checking_no_of_1_in_binary.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,count=0,temp;
+    /* taaking user input */
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    temp = num;
+    /* while loop to count number of 1s in an integer */
+    while(num>0)
+    {
+        if(num&1!=0)
+            count++;
+        num=num/2;
+    }
+    printf("\nNumber of 1s in %d: %d",temp,count);
+
+    return 0;
+}
